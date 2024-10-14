d532134b3d2edf7f1b1ddcc4a6a0a765a64cdf0a
sude <sude.bayar@student.htldornbirn.at>
2024-10-14 14:21:09 +0200

    printer und main

---
+++ src/at/bas/projects/ticket/Main.java
@@ -0,0 +1,10 @@
+package at.bas.projects.ticket;
+
+public class Main {
+    public static void main(String[] args) {
+        Printer printer = new Printer();
+        Ticket ticket = printer.PrintTicket();
+        System.out.println("\n id: " + ticket.getId() + " \n created: " + ticket.getCreatedTimeStamp() + " \n payed: "
+                + ticket.getPayedTimeStamp());
+    }
+}
---
+++ src/at/bas/projects/ticket/Printer.java
@@ -0,0 +1,10 @@
+package at.bas.projects.ticket;
+
+public class Printer {
+
+    public Ticket PrintTicket() {
+        Ticket ticket = new Ticket();
+
+        return ticket;
+    }
+}
