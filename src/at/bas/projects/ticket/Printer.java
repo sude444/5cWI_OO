c35bbce6971f201d8b7b8221669056791ca995d0
sude <sude.bayar@student.htldornbirn.at>
2024-10-16 12:44:59 +0200

    cash

--- src/at/bas/projects/ticket/Printer.java
+++ src/at/bas/projects/ticket/Printer.java
@@ -4,7 +4,8 @@ public class Printer {
 
     public Ticket PrintTicket() {
         Ticket ticket = new Ticket();
-
+        System.out.println("\n id: " + ticket.getId() + " \n created: " + ticket.getCreatedTimeStamp() + " \n payed: "
+                + ticket.getPayedTimeStamp());
         return ticket;
     }
 }
